64dd974414cc22cfe67b31935288b67e14713b9f
Lm Only <luccamoraes7890@gmail.com>
2026-09-02 11:37:26 -0400

    up

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * @license MIT
+ * 
+ * Copyright 2026 Yuta & Hutao bot community
+ * Yuta APIs - Em parceria com HutaoBot
+ *
+ * @author Lm Only and Nk Petrov
+ */
+ 
 import { YutaApis } from './client.js';
 export type { YutaApisOptions } from './client.js';
 
